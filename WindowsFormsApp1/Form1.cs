0d4ad785b8ad2174780980d37e0488ca18e2f38c
翔宇 吴 <wuxiangyu@tju.edu.cn>
2018-01-24 18:31:31 +0800

    20180124

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1;
+
+namespace WindowsFormsApp1
+{
+    public partial class winform : Form
+    {
+       static public bool sign;
+        static public string inputstring;
+        public string[] arr;
+
+        public winform()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //serialPort1.Open();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+ 
+        }
+
+     
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void _isSerialOpen_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+                MessageBox.Show("open");
+            else
+                MessageBox.Show("close");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Form2 frm2 = new Form2();
+            //frm2.Show();
+           if(!serialPort1 .IsOpen)
+                serialPort1.Open();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort1.ReadTo(arr[0]);
+            textBox1.Text = arr[0];
+        }
+    }
+}
---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+   public static class Program
+    {
+        static SerialPort _serialPort;
+    
+       // static string shou;
+        
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+       public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new winform());
+
+           _serialPort = new SerialPort();
+            
+            //  _serialPort.Open();
+           
+            winform.sign = _serialPort.IsOpen;
+                
+        }
+    }
+}
